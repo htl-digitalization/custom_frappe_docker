33a8a33f6379af1b567d30e63b530d3a0be1a482
HieuTrLuu <thl1g15@outlook.com>
2022-10-19 10:14:44 +0700

    use app tpp_erpnext

--- ci/clone-apps.sh
+++ ci/clone-apps.sh
@@ -21,4 +21,4 @@
 git clone --depth 1 --branch master https://github.com/yrestom/POS-Awesome repos/posawesome
 git clone --depth 1 --branch master https://github.com/frappe/wiki repos/wiki
 # git clone --depth 1 --branch main https://github.com/htl-digitalization/corrugate_core.git repos/corrugate_core
-git clone --depth 1 --branch main https://hieutrluu:${PERSONAL_ACCESS_TOKEN}@github.com/htl-digitalization/corrugate_core repos/corrugate_core
+git clone --depth 1 --branch main https://hieutrluu:${PERSONAL_ACCESS_TOKEN}@github.com/htl-digitalization/tpp_erpnext repos/tpp_erpnext
